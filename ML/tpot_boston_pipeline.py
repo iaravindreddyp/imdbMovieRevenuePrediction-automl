7d747dcfd23750f4997d31d8010d294073e0ddf9
Aravind Reddy <reddy.aravind178@gmail.com>
2019-02-20 22:23:51 +0530

    autoklearn

--- ML/tpot_boston_pipeline.py
+++ ML/tpot_boston_pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
@@ -9,8 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=None)
 
-# Average CV score on the training set was:-1.53261418701e+15
-exported_pipeline = RandomForestRegressor(bootstrap=False, max_features=0.5, min_samples_leaf=4, min_samples_split=14, n_estimators=100)
+# Average CV score on the training set was:-1.81965801871e+15
+exported_pipeline = GradientBoostingRegressor(alpha=0.75, learning_rate=0.1, loss="huber", max_depth=6, max_features=0.75, min_samples_leaf=5, min_samples_split=20, n_estimators=100, subsample=0.65)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
